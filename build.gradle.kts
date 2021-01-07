336fbe32a9673d68a14b220145b741c21e8a5ce7
Marcos Almeida <marcos@marcosalmeida.dev>
2021-01-07 17:33:08 +0100

    chore: use kotlin files for gradle

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("org.springframework.boot") version "2.4.1"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    `java`
+}
+
+group = "dev.marcosalmeida"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("mysql:mysql-connector-java")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
