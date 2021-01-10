b5fab14d954135356fc1793d2efe52d3eab16261
Marcos Almeida <marcos@marcosalmeida.dev>
2021-01-10 15:48:48 +0100

    chore: add error handler

--- src/main/java/dev/marcosalmeida/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/dev/marcosalmeida/msscbeerservice/web/controller/BeerController.java
@@ -3,6 +3,7 @@ package dev.marcosalmeida.msscbeerservice.web.controller;
 import dev.marcosalmeida.msscbeerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -22,10 +23,16 @@ public class BeerController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public ResponseEntity createBeer(@RequestBody BeerDto beer) {
+    public ResponseEntity createBeer(@Validated @RequestBody BeerDto beer) {
         //TODO Implement services
 
         return ResponseEntity.created(URI.create("/api/v1/beer/")).build();
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void updateBeer(@PathVariable("id") UUID id, @Validated @RequestBody BeerDto beer) {
+        
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
