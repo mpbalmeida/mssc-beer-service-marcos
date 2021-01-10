2a3559c5f40f64c29ca9d6f3beb9a362405ab527
Marcos Almeida <marcos@marcosalmeida.dev>
2021-01-10 15:40:09 +0100

    feat: add bean validation

--- src/main/java/dev/marcosalmeida/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/dev/marcosalmeida/msscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
@@ -14,13 +18,32 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+
+    @Null
     private UUID id;
+
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @NotNull
+    @Positive
     private Long upc;
+    
+    @NotNull
+    @Positive
     private BigDecimal price;
-    private Integer version;
+
     private Integer quantityOnHand;
+
+    @Null
+    private Integer version;
+
+    @Null
     private Instant createdDate;
+
+    @Null
     private Instant lastModifiedDate;
 }
