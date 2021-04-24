dee1a82d7134e49ceb220a5e010e385a21c8cee1
Marcos Almeida <marcos@marcosalmeida.dev>
2021-04-25 00:51:49 +0200

    chore: use bom to control the versions

--- src/main/java/dev/marcosalmeida/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/dev/marcosalmeida/msscbeerservice/bootstrap/BeerLoader.java
@@ -4,19 +4,20 @@ import dev.marcosalmeida.msscbeerservice.domain.Beer;
 import dev.marcosalmeida.msscbeerservice.repository.BeerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @RequiredArgsConstructor
 // Removing bootstrap to give place to data.sql in order to have fixed ids for http test
-//@Component
+@Component
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
     public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
-    
+
     public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
     public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
     public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
@@ -25,8 +26,8 @@ public class BeerLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//        loadBeerObjects();
-        loadBeerPerformanceObjects();
+        loadBeerObjects();
+//        loadBeerPerformanceObjects();
     }
 
     private void loadBeerPerformanceObjects() {
